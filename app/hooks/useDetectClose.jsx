be035c2b32c26fd476bb96d3f022bedd1dbedc87
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-06-28 15:56:32 +0900

    🎨 body 추가

---
+++ app/hooks/useDetectClose.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useDetectClose = (elem, initialState) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  useEffect(() => {
+    const onClick = (e) => {
+      if (elem.current && !elem.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("click", onClick);
+    }
+
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, [isOpen, elem]);
+
+  return [isOpen, setIsOpen];
+};
+
+export default useDetectClose;
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,15 @@
 "use client";
 import ReservationModal from "./components/ReservationModal";
+import useDetectClose from "./hooks/useDetectClose";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const dropDownRef = useRef(null);
+    const [isOpen, setIsOpen] = useDetectClose(dropDownRef, false);
+    
     const handleClick = () => {
         setIsModalOpen(true);
     };
@@ -14,9 +17,37 @@ export default function Home() {
     let ex = ["hi", "hi2"];
     return (
         <>
-            <button className="px-4 py-2 rounded bg-blue text-sky-500 hover:text-sky-500" onClick={handleClick}>
-                예약하기
-            </button>
+            <section className="bg-[var(--bg-main-color)] flex-1">
+                <div className="mt-12 container--default">
+                    <div className="relative flex flex-wrap w-full gap-4 mb-8 container__inner">
+                        <div className="flex flex-col w-full gap-4 h-60 md:w-1/2 lg:w-1/3">
+                            <div className="flex-1 w-full ">
+                                <span>2024.07.06 (월)</span>
+                                <span>오후 1: 50</span>
+                            </div>
+                            <div className="flex-1 w-full bg-white shadow-3xl ">
+                                <div className="dropDownMenu">
+                                    <button onClick={() => setIsOpen(!isOpen)}>메뉴 보기</button>
+
+                                    <ul ref={dropDownRef} className={`menu ${isOpen ? "menu--open" : ""}`}>
+                                        <li>
+                                            <Link href="/mypage">마이페이지</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex-1 w-full bg-white h-60 md:w-1/2 lg:w-2/3 shadow-3xl ">
+                            <button className="px-4 py-2 rounded bg-blue text-sky-500 hover:text-sky-500" onClick={handleClick}>
+                                예약하기
+                            </button>
+                        </div>
+                        <div className="w-full bg-white shadow-3xl h-60 "></div>
+                    </div>
+                </div>
+            </section>
+
             {/* <Modal isOpen={isModalOpen} onClose={handleCloseModal}></Modal> */}
             <ReservationModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} hi={ex[0]} />
         </>
