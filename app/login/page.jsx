67b679a2aaaa0020face02c1afaba1b6e9f4647a
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-07-05 10:12:15 +0900

    회원가입 페이지 추가

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Login() {
     return (
         <>
-            <div className="flex bg-[var(--bg-main-color)] flex-1 py-[60px]">
+            <div className="flex bg-[var(--bg-main-color)] flex-1 mt-[100px]">
                 <div className="z-10 w-full mt-[0px] pb-[110px] md:my-[0px] md:pb-[0px]">
                     <div className="p-10 py-[60px] mx-auto bg-white shadow-3xl rounded-lg px-6 md:px-8 md:w-3/4 lg:w-[600px] w-[92%]">
                         <h2 className="mb-10 text-2xl font-bold text-center text-[#222]">로그인</h2>
@@ -32,7 +32,7 @@ export default function Login() {
                                         이메일
                                     </label>
                                     <input type="text" id="email3" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm text-red-500 border border-red-500 rounded-lg outline-none bg-white/0 placeholder:text-red-500 "></input>
-                                    <p className="mt-2 text-sm text-red-400">이메일 또는 비밀번호가 일치하지 않습니다.</p>
+                                    <p className="mt-2 text-sm text-red-400">이메일이 일치하지 않습니다.</p>
                                 </div>
                                 {/* 비밀번호 입력 */}
                                 <div className="mb-3">
@@ -40,14 +40,6 @@ export default function Login() {
                                         비밀번호
                                     </label>
                                     <input type="password" name="password" id="password" placeholder="비밀번호" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
-                                </div>
-                                {/* 비밀번호 실패 */}
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
-                                        비밀번호
-                                    </label>
-                                    <input type="password" name="password" id="password" placeholder="비밀번호" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm text-red-500 border border-red-500 rounded-lg outline-none bg-white/0 placeholder:text-red-500 "></input>
-                                    <p className="mt-2 text-sm text-red-400">영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.</p>
                                 </div>
                                 {/* 자동 로그인 체크박스 */}
                                 <div className="check-wrap">
@@ -59,22 +51,22 @@ export default function Login() {
                                 </div>
                             </div>
                             {/* 로그인 */}
-                            <button className="block w-full p-3 font-bold font-md text-white bg-[#4e5968]  rounded-lg mt-10">로그인</button>
+                            <button className="block w-full p-3 font-bold font-md text-white bg-[#4e5968]  rounded-lg mt-8 hover:bg-[#4e5968d1] transition duration-300 ease-in-out">로그인</button>
 
                             {/* 구글 로그인 */}
-                            <button className="flex items-center justify-center w-full p-[0.4375rem] font-bold font-md text-[#4e5968] border border-gray-300 bg-[#fff] rounded-lg mt-3">
+                            <button className="flex items-center justify-center w-full p-[0.4375rem] font-bold font-md text-[#4e5968] border hover:text-[#4e5968d1] hover:border-[#4e5968d1] border-gray-300 bg-[#fff] rounded-lg mt-3 transition duration-300 ease-in-out">
                                 <Image className="" alt="구글 아이콘" src="/assets/google-icon.svg" width={32} height={32} />
                                 구글 계정으로 로그인
                             </button>
 
                             <div>
-                                <Link href="/login/password" className="block mx-auto mt-10 text-sm font-semibold leading-6 text-center text-gray-600 underline-link w-fit">
+                                <Link href="/login/password" className="block mx-auto mt-10 text-sm font-semibold leading-6 text-center text-gray-600 underline-link w-fit transition duration-300 ease-in-out hover:text-[#4e5968d1]">
                                     비밀번호를 잊으셨나요?
                                 </Link>
 
                                 <div className="flex justify-center mt-5 item-center">
                                     <p className="font-semibold pr-1.5 text-gray-400 text-sm">계정이 없으신가요?</p>
-                                    <Link href="/login/password" className="text-sm font-semibold text-gray-600 underline-link">
+                                    <Link href="/signup" className="text-sm font-semibold text-gray-600 underline-link transition duration-300 ease-in-out hover:text-[#4e5968d1] ">
                                         가입하기
                                     </Link>
                                 </div>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,8 +1,96 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Signup() {
     return (
-      <div>
-        <h4 className="title">Signup</h4>
-       
-      </div>
-    )
-  } 
+        <>
+            <div className="flex bg-[var(--bg-main-color)] flex-1 mt-[100px]">
+                <div className="z-10 w-full mt-[0px] pb-[110px] md:my-[0px] md:pb-[0px]">
+                    <div className="p-10 py-[60px] mx-auto bg-white shadow-3xl rounded-lg px-6 md:px-8 md:w-3/4 lg:w-[600px] w-[92%]">
+                        <h2 className="mb-10 text-2xl font-bold text-center text-[#222]">회원가입</h2>
+                        <form action="">
+                            <div className="">
+                                <div className="relative flex flex-row justify-between"></div>
+                                {/* 이메일 입력 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        이메일
+                                    </label>
+                                    <input type="email" name="email" id="email" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                                </div>
+                                {/* 성공 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email2" className="text-sm font-bold text-navy-700 ">
+                                        이메일
+                                    </label>
+                                    <input type="text" id="email2" placeholder="이메일" className="mt-2 flex h-12 w-full items-center justify-center rounded-lg border bg-white/0 p-3 text-sm outline-none border-[#5C92FF] text-[#5C92FF] placeholder:text-[#5C92FF] "></input>
+                                </div>
+                                {/* 실패 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email3" className="text-sm font-bold text-navy-700 ">
+                                        이메일
+                                    </label>
+                                    <input type="text" id="email3" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm text-red-500 border border-red-500 rounded-lg outline-none bg-white/0 placeholder:text-red-500 "></input>
+                                    <p className="mt-2 text-sm text-red-400">이메일이 일치하지 않습니다.</p>
+                                </div>
+                                {/* 이메일 인증하기 */}
+                                <button className="block w-full my-8 transition duration-300 ease-in-out font-md gray__btn">이메일 인증하기</button>
+                                {/* 이메일 인증하기  - 클릭 활성화*/}
+                                <button className="block w-full my-8 transition duration-300 ease-in-out font-md blue__btn">이메일 인증하기</button>
+
+                                {/* 비밀번호 입력 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        비밀번호
+                                    </label>
+                                    <input type="password" name="password" id="password" placeholder="비밀번호" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                                </div>
+                                {/* 비밀번호 실패 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        비밀번호
+                                    </label>
+                                    <input type="password" name="password" id="password" placeholder="비밀번호" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm text-red-500 border border-red-500 rounded-lg outline-none bg-white/0 placeholder:text-red-500 "></input>
+                                    <p className="mt-2 text-sm text-red-400">영문, 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해주세요.</p>
+                                </div>
+
+                                {/* 이름 입력 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        이름
+                                    </label>
+                                    <input type="text" placeholder="이름" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                                </div>
+                                {/* 부서명 입력 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        부서명
+                                    </label>
+                                    <input type="text" placeholder="이름" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                                </div>
+                                {/* 부서명 입력 */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                        부서명
+                                    </label>
+                                    <input type="text" placeholder="이름" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                                </div>
+                            </div>
+                            {/* 로그인 */}
+                            <button className="block w-full p-3 font-bold text-white bg-[#4e5968]  rounded-lg mt-8 hover:bg-[#4e5968d1] transition duration-300 ease-in-out">로그인</button>
+
+                            <div>
+                                <div className="flex justify-center mt-5 item-center">
+                                    <p className="font-semibold pr-1.5 text-gray-400 text-sm">이미 아이디가 있으신가요? </p>
+                                    <Link href="/login" className="text-sm font-semibold text-gray-600 underline-link transition duration-300 ease-in-out hover:text-[#4e5968d1] ">
+                                        로그인
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
