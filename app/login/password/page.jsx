f4d3a23b97bc269d4913a7678389366024c80f9f
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-07-05 16:16:32 +0900

    이메일 실패 추가

--- app/login/password/page.jsx
+++ app/login/password/page.jsx
@@ -23,22 +23,35 @@ export default function Password() {
                         <h2 className="mb-3 text-2xl font-bold text-center text-[#222]">비밀번호 찾기</h2>
                         <p className="mb-10 text-sm text-center text-gray-500 font-nomal">가입하신 이메일 주소를 입력해주세요.</p>
                         <form action="">
-                            <div className="">
-                                <div className="relative flex flex-row justify-between"></div>
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
-                                        이메일
-                                    </label>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                    이메일
+                                </label>
 
-                                    <input type="email" name="email" id="email" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
-                                </div>
+                                <input type="email" name="email" id="email" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm border border-gray-300 rounded-lg outline-none bg-white/0"></input>
+                            </div>
+                            {/* 실패 */}
+                            <div className="mb-3">
+                                <label htmlFor="email" className="text-sm font-bold text-navy-700 ">
+                                    이메일
+                                </label>
+
+                                <input type="email" name="email" id="email" placeholder="이메일" className="flex items-center justify-center w-full h-12 p-3 mt-2 text-sm text-red-500 border border-red-500 rounded-lg outline-none bg-white/0 placeholder:text-red-500 "></input>
+                                <p className="mt-2 text-sm text-red-400">이메일이 일치하지 않습니다.</p>
                             </div>
                         </form>
                         <button className="block w-full p-3 font-bold font-md text-white bg-[#4e5968] rounded-lg mt-10" onClick={() => setIsModalOpen(true)}>
                             비밀번호 찾기
                         </button>
-                        <CommonModal open={isModalOpen} setOpen={setIsModalOpen} title="example@example.com 메일로 
-인증코드를 전송하였습니다." content="이메일이 수신되지 않는 경우 입력하신 이메일 주소가 정확한지 또는 스팸 메일함을 확인해주세요." onConfirm={handleConfirm} singleButton={true} />{" "}
+                        <CommonModal
+                            open={isModalOpen}
+                            setOpen={setIsModalOpen}
+                            title="example@example.com 메일로 
+인증코드를 전송하였습니다."
+                            content="이메일이 수신되지 않는 경우 입력하신 이메일 주소가 정확한지 또는 스팸 메일함을 확인해주세요."
+                            onConfirm={handleConfirm}
+                            singleButton={true}
+                        />{" "}
                     </div>
                 </div>
             </div>
